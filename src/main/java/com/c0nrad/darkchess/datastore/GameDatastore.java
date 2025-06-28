19cf2a12f0831ef5decaf70d04e3b3de3aed24b8
gituserces <81975815+gituserces@users.noreply.github.com>
2025-06-28 20:18:24 -0300

    Bugfix constantes, todos los niveles (GameDatastore.java)
    Se aplica el uso de constantes para todos los niveles del juego.

--- src/main/java/com/c0nrad/darkchess/datastore/GameDatastore.java
+++ src/main/java/com/c0nrad/darkchess/datastore/GameDatastore.java
@@ -2,30 +2,163 @@ package com.c0nrad.darkchess.datastore;
 
 import java.util.List;
 
-import com.c0nrad.darkchess.models.Game;
+import com.c0nrad.darkchess.exceptions.InvalidBoardException;
+import com.c0nrad.darkchess.exceptions.InvalidPieceException;
+import com.c0nrad.darkchess.models.Board;
+import com.c0nrad.darkchess.models.PlayerType;
+import com.c0nrad.darkchess.models.Story;
 
 import org.bson.types.ObjectId;
 import xyz.morphia.Datastore;
 
-public class GameDatastore {
-    public static Game Find(String gameId) {
+public class StoryDatastore {
+    private static final String WHITE_PAWN_ROW = "PPPPPPPP";
+    private static final String BLACK_PAWN_ROW = "pppppppp";
+    private static final String EMPTY_ROW      = "        ";
+    private static final String WHITE_KING_ROW = "    K   ";
+    private static final String BLACK_KING_ROW = "    k   ";
+    
+    public static Story Find(String storyId) {
         Datastore d = MorphiaSingleton.GetDatastore();
-        ObjectId objectId = new ObjectId(gameId);
-        return d.get(Game.class, objectId);
+        ObjectId objectId = new ObjectId(storyId);
+        return d.get(Story.class, objectId);
+    }
+    
+
+    public static List<Story> FindAll() {
+        Datastore d = MorphiaSingleton.GetDatastore();
+        return d.createQuery(Story.class).asList();
     }
 
-    public static void Update(Game g) {
+    public static Story FindByLevel(int level) {
         Datastore d = MorphiaSingleton.GetDatastore();
-        d.merge(g);
+        return d.createQuery(Story.class).field("level").equal(level).get();
     }
 
-    public static void Save(Game g) {
+    public static void SeedLevels() throws InvalidBoardException, InvalidPieceException{
         Datastore d = MorphiaSingleton.GetDatastore();
-        d.save(g);    
-    }
+        
+        Story s1 = new Story();
+        s1.title = "Frontal Siege";
+        s1.level = 1;
+        s1.description = "Commander! A wall of pawns is approaching!\n" +
+        "\n"+
+        "Can you break through the wall of pawns and kill the opposing commander?\n" +
+        "\n"+
+        "The fate of our people depends on you.";
 
-    public static List<Game> FindAll() {
-        Datastore d = MorphiaSingleton.GetDatastore();
-        return d.createQuery(Game.class).asList();     
+    String[] layout = {
+    WHITE_KING_ROW,
+    WHITE_PAWN_ROW,
+    EMPTY_ROW,
+    EMPTY_ROW,
+    EMPTY_ROW,
+    EMPTY_ROW,
+    BLACK_PAWN_ROW,
+    BLACK_KING_ROW
+};
+
+        Board b = new Board(layout);
+
+        s1.startingboard = b;
+        s1.blackPlayer = PlayerType.RANDBOT;
+        d.save(s1);
+
+
+        //---------- LEVEL 2 ---------
+        Story s2 = new Story();
+        s2.title = "Assassins Dance";
+        s2.level = 2;
+        s2.description = "Commander we believe there will be an assassination attempt on your life.\n" +
+        "\n"+
+        "Can you find and kill the enemy king before being assassinated??\n";
+
+        String[] layout2 ={
+                "   NKN  ", 
+        EMPTY_ROW,
+        EMPTY_ROW,
+        EMPTY_ROW,
+        EMPTY_ROW,
+        EMPTY_ROW,
+        EMPTY_ROW,
+                "   nkn  "}; 
+
+
+        Board b2 = new Board(layout2);
+
+        s2.startingboard = b2;
+        s2.blackPlayer = PlayerType.KILLBOT;
+        d.save(s2);
+
+        //---------- LEVEL 3 ---------
+        Story s3 = new Story();
+        s3.title = "The kidnapped queen";
+        s3.level = 3;
+        s3.description = "Commander! The queen has been kidnapped! Launch a full frontal siege and kill the enemey king to resecure the queen.!\n" +
+        "\n"+
+        "Hurry before it's too late!?\n";
+
+        String[] layout3 ={
+                "RNBQKBNR", 
+                WHITE_PAWN_ROW,
+                EMPTY_ROW,
+                EMPTY_ROW,
+                EMPTY_ROW,
+                EMPTY_ROW,
+                BLACK_PAWN_ROW,
+                "rnb kbnr"}; 
+
+        Board b3 = new Board(layout3);
+
+        s3.startingboard = b3;
+        s3.blackPlayer = PlayerType.RANDBOT;
+        d.save(s3);
+
+        //---------- LEVEL 4 ---------
+        Story s4 = new Story();
+        s4.title = "Peasant Uprising";
+        s4.level = 4;
+        s4.description = "Commander! The villagers are angry about the recent raise in taxes and are coming to kill the nobles!\n" +
+        "\n"+
+        "Kill the peasants and their leader before they get to us!?\n";
+
+        String[] layout4 ={
+        	WHITE_PAWN_ROW, 
+        	WHITE_PAWN_ROW,
+        	WHITE_PAWN_ROW,
+        	EMPTY_ROW,
+        	EMPTY_ROW,
+        	EMPTY_ROW,
+        	EMPTY_ROW,
+            "rnbqkbnr"}; 
+
+        Board b4 = new Board(layout4);
+
+        s4.startingboard = b4;
+        s4.blackPlayer = PlayerType.PAWNPUSHERBOT;
+        d.save(s4);
+
+        //---------- LEVEL 5 ---------
+        Story s5 = new Story();
+        s5.title = "Final Fight";
+        s5.level = 5;
+        s5.description = "Commander. This is it. We've recieved word from our scouts that the strong nation in the lands is trying to take over our village. Can you defend the city?\n";
+
+        String[] layout5 ={
+            "RNBQKBNR", 
+            WHITE_PAWN_ROW,
+            EMPTY_ROW,
+            EMPTY_ROW,
+            EMPTY_ROW,
+            EMPTY_ROW,
+            BLACK_PAWN_ROW,
+            "rnbqkbnr"}; 
+
+        Board b5 = new Board(layout5);
+
+        s5.startingboard = b5;
+        s5.blackPlayer = PlayerType.KILLBOT;
+        d.save(s5);
+
     }
 }
